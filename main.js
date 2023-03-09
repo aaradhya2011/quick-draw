5afecb8d36047473fd96f1cce5fc6df6217faaa4
aaradhya2011 <80405586+aaradhya2011@users.noreply.github.com>
2023-03-09 10:05:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,52 @@ var timer_check=""
 var drawn_sketch=""
 var answer_holder="0"
 var score=""
-random_number = Math.floor((Math.random()*array_1.length)+1)
-Element_of_array = quick_draw_data_set[random_number]
 console.log()
+
+function draw()
+{
+    check_sketch()
+
+if(drawn_sketch==sketch)
+{
+    answer_holder="set"
+    score++;
+    document.getElementById("score").innerHTML="Score: "+score
+}
+
+}
+
+function update_canvas()
+{
+    background("white")
+    random_number = Math.floor((Math.random()*quick_draw_data_set.length)+1);
+    console.log(quick_draw_data_set[random_number]);
+    sketch=quick_draw_data_set[random_number];
+    document.getElementById("sketch_name").innerHTML="sketch to be drawn "+sketch;
+}
+
+function setup()
+{
+    canvas=createCanvas(280,280)
+    canvas.center()
+    background("white")
+}
+
+check_sketch()
+{
+    timer_counter++
+    document.getElementById("time").innerHTML="Timer: "+Timer_counter
+    console.log(timer_counter)
+
+if(timer_counter>400)
+{
+    timer_counter=0
+}
+
+if(timer_check=="completed" || answer_holder=="set")
+{
+    timer_check=""
+    answer_holder=""
+    update_canvas()
+}
+}
